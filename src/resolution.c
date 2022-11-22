e1b5546c9616579fd1e58f6b0c2ae031eaaac3e7
Umberto <>
2022-11-22 16:03:40 +0100

    d

--- src/resolution.c
+++ src/resolution.c
@@ -1,16 +1,27 @@
 #include "ft_ping.h"
 
+void    config_socket(int sockfd, const int *opts)
+{
+    int hold = 1;
+    struct timeval tv = {
+            .tv_sec = 2,
+            .tv_usec = 0,
+    };
+
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+    setsockopt(sockfd, SOL_IP, IP_RECVERR, &hold, sizeof(hold));
+    if (opts['d'])
+        setsockopt(sockfd, SOL_SOCKET, SO_DEBUG, &hold, sizeof(hold));
+    if (opts['t'])
+        setsockopt(sockfd, IPPROTO_IP, IP_TTL, &opts['t'], sizeof(int));
+}
+
 int     resolve_hostname(const char *av, const int *opts, char *ip, struct sockaddr_in *dst)
 {
     int error = 0, sockfd = 0;
     struct addrinfo hints = {0};
     struct addrinfo *addrinfo_list = 0, *tmp = 0;
     struct sockaddr_storage addr;
-    int hold = 1;
-    struct timeval tv = {
-            .tv_sec = 2,
-            .tv_usec = 0,
-    };
 
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_RAW;
@@ -28,12 +39,7 @@ int     resolve_hostname(const char *av, const int *opts, char *ip, struct socka
     if (sockfd < 0 || !tmp)
         return -1;
 
-    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
-    setsockopt(sockfd, SOL_IP, IP_RECVERR, &hold, sizeof(hold));
-    if (opts['d'])
-        setsockopt(sockfd, SOL_SOCKET, SO_DEBUG, &hold, sizeof(hold));
-    if (opts['t'])
-        setsockopt(sockfd, IPPROTO_IP, IP_TTL, &opts['t'], sizeof(int));
+    config_socket(sockfd, opts);
     memcpy(&addr, tmp->ai_addr, tmp->ai_addrlen);
     inet_ntop(addr.ss_family, (void *)&((struct sockaddr_in *)&addr)->sin_addr, ip, INET_ADDRSTRLEN);
     memcpy(&(dst->sin_addr), &((struct sockaddr_in *)&addr)->sin_addr, sizeof(struct in_addr));
